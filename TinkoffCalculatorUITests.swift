733ed2132cdabd76b123f061e544c808afbf5c11
i.myakotin <i.myakotin@tinkoff.ru>
2023-10-24 12:49:29 +0300

    Init

---
+++ TinkoffCalculatorUITests.swift
@@ -0,0 +1,37 @@
+//
+//  TinkoffCalculatorUITests.swift
+//  TinkoffCalculatorUITests
+//
+//  Created by r.r.amirova on 16.02.2022.
+//
+
+import XCTest
+
+class TinkoffCalculatorUITests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+    }
+
+    override func tearDown() {
+    }
+
+    func test_lecture12_calculations() {
+        let app = XCUIApplication()
+        app.launch()
+
+        app.buttons["2"].tap()
+        app.buttons["7"].tap()
+        app.buttons["+"].tap()
+        app.buttons["3"].tap()
+        app.buttons["="].tap()
+
+        app.buttons["toHistoryPageButton"].tap()
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        let dateString = dateFormatter.string(from: Date())
+
+        XCTAssertTrue(app.tables.firstMatch.otherElements.containing(.staticText, identifier: dateString).firstMatch.exists)
+    }
+}
